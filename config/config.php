f5d0d69deeeb3a6d9a0f2a4db31b9b17f4cb66ea
Ponomarenko <sergey.ponomarenko.stratis@gmail.com>
2024-05-14 21:51:52 +0300

    [TASK] db setting

--- config/config.php
+++ config/config.php
@@ -4,12 +4,22 @@ use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '../../');
 $dotenv->load();
+
 $host = $_ENV['DB_HOST'];
-$dbname = $_ENV['DB_NAME'];
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_DATABASE'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$port = $_ENV['DB_PORT'];
+
+var_dump($host);
+var_dump($dbname);
+var_dump($username);
+var_dump($password);
+var_dump($port);
+
+
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "connection success";

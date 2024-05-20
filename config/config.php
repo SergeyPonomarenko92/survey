e745b8fdd7ac389163c5f41bac80b790167a237c
Ponomarenko <sergey.ponomarenko.stratis@gmail.com>
2024-05-20 21:47:05 +0300

    [TASK] Routes settings

--- config/config.php
+++ config/config.php
@@ -4,6 +4,10 @@ use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '../../');
 $dotenv->load();
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
 
 $host = $_ENV['DB_HOST'];
 $dbname = $_ENV['DB_DATABASE'];
@@ -11,17 +15,10 @@ $username = $_ENV['DB_USERNAME'];
 $password = $_ENV['DB_PASSWORD'];
 $port = $_ENV['DB_PORT'];
 
-var_dump($host);
-var_dump($dbname);
-var_dump($username);
-var_dump($password);
-var_dump($port);
-
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     echo "connection success";
 } catch (PDOException $e) {
     die("conection failed " . $e->getMessage());
--- public/index.php
+++ public/index.php
@@ -1,11 +1,14 @@
 <?php
 
 
+
 require_once __DIR__ . '/../vendor/autoload.php'; // include autoload
 
 // Config, database settings
 require_once '../config/config.php';
 
+
 //// Routes
-//require_once '../routes/web.php';
-//require_once '../app/Router.php';
+require_once '../routes/web.php';
+require_once '../app/Router.php';
+
